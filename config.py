d331d1568dadcbbe40b20c3c3f9dc214a6439415
Anupam965836 <anupamsarkar466@gmail.com>
2025-01-23 20:38:11 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = "7906641255:AAEX1op_S5PYFoHrpjcjrslsWpyaOX8KeW4"
+ADMIN_IDS = ["1944182800"]
+GROUP_ID = -1002365252810  # Replace with your group ID
+GROUP_LINK = "https://t.me/SarkarBhaiPublicbot"  # Replace with your group invite link
+# Default number of threads for attacks
+DEFAULT_THREADS = 30  # Set the default number of threads to use for attacks
